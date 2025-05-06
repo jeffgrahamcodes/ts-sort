ae5c8bd82da645e2bd2d6eee89370420c6ebb2ce
jeffgrahamcodes <jeffgrahamcodes@gmail.com>
2025-05-06 16:21:03 -0400

    chore: create Linkedlist collection

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
 import { CharactersCollection } from './CharactersCollection';
+import { LinkedList } from './LinkedList';
 
 const numbersCollection = new NumbersCollection([
   10000, 3, -5, 10, 1,
@@ -9,7 +10,16 @@ const sorter = new Sorter(numbersCollection);
 sorter.sort();
 console.log(numbersCollection.data);
 
-const charactersCollections = new CharactersCollection('Xenophobe');
+const charactersCollections = new CharactersCollection('Apple');
 const charSorter = new Sorter(charactersCollections);
 charSorter.sort();
 console.log(charactersCollections.data);
+
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
+const llSorter = new Sorter(linkedList);
+llSorter.sort();
+linkedList.print();
