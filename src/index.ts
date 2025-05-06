55fa2749e280f86f3ef670b35e8d9e90f601bea5
jeffgrahamcodes <jeffgrahamcodes@gmail.com>
2025-05-06 15:31:17 -0400

    chore: create CharactersCollection

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
+import { CharactersCollection } from './CharactersCollection';
 
 const numbersCollection = new NumbersCollection([
   10000, 3, -5, 10, 1,
@@ -7,3 +8,8 @@ const numbersCollection = new NumbersCollection([
 const sorter = new Sorter(numbersCollection);
 sorter.sort();
 console.log(numbersCollection.data);
+
+const charactersCollections = new CharactersCollection('Xenophobe');
+const charSorter = new Sorter(charactersCollections);
+charSorter.sort();
+console.log(charactersCollections.data);
