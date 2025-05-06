6e72c0696d23ddb1c25e62e1df8a53e6237fe52c
jeffgrahamcodes <jeffgrahamcodes@gmail.com>
2025-05-06 15:18:16 -0400

    chore: create interface

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
 
-const numbersCollection = new NumbersCollection([10000, 3, -5, 10]);
+const numbersCollection = new NumbersCollection([
+  10000, 3, -5, 10, 1,
+]);
 const sorter = new Sorter(numbersCollection);
 sorter.sort();
 console.log(numbersCollection.data);
