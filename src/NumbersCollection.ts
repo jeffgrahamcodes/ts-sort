df2a5772e7b5206950cc8627830b400d0c11ee1d
jeffgrahamcodes <jeffgrahamcodes@gmail.com>
2025-05-16 12:59:45 -0400

    chore: create abstract Sorter class

--- src/NumbersCollection.ts
+++ src/NumbersCollection.ts
@@ -9,9 +9,9 @@ export class NumbersCollection extends Sorter {
     return this.data.length;
   }
 
-  compare(leftIndex: number, rightIndex: number): boolean {
-    return this.data[leftIndex] > this.data[rightIndex];
-  }
+  // compare(leftIndex: number, rightIndex: number): boolean {
+  //   return this.data[leftIndex] > this.data[rightIndex];
+  // }
 
   swap(leftIndex: number, rightIndex: number): void {
     const temp = this.data[leftIndex];
